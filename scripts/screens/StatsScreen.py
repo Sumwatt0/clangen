a1045e809c6f7c7991899de06fa6497bc5aee462
Sumwatt <172867164+Sumwatt0@users.noreply.github.com>
2024-07-31 00:06:46 -0400

    Added Main Menu Button To Stats Screens
    Added the main menu button to the stats screen.

--- scripts/screens/StatsScreen.py
+++ scripts/screens/StatsScreen.py
@@ -19,17 +19,24 @@ class StatsScreen(Screens):
     """
     def screen_switches(self):
         """Runs when this screen is switched to."""
-        pass
+        self.main_menu_button = UIImageButton(scale(
+            pygame.Rect((50, 50), (305, 60))),
+            "",
+            object_id="#main_menu_button",
+            manager=MANAGER)
 
     def handle_event(self, event):
         """This is where events that occur on this page are handled.
         For the pygame_gui rewrite, button presses are also handled here. """
-        pass
+        if event.type == pygame_gui.UI_BUTTON_START_PRESS:
+            if event.ui_element == self.main_menu_button:
+                self.change_screen('start screen')
+                return
 
     def exit_screen(self):
         """Runs when screen exits"""
-        pass
+        self.main_menu_button.kill()
+        del self.main_menu_button
 
     def on_use(self):
         """Runs every frame this screen is used."""
-        pass
